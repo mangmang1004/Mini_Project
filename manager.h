60942941c17b2da7dd8bd66606cbbdd1f902ffd5
mangmang1004 <22000669@handong.ac.kr>
2022-04-18 20:57:37 +0900

    adding function 'loadData' in manager.h

--- manager.h
+++ manager.h
@@ -20,4 +20,4 @@ void listProducts(Product *s,int count); //등록되어 있는 제품의 전체 
 int selectDataNo(Product *s, int count); // 원하는 번호를 선택하는 함수
 void searchName(Product *s, int count); // 제품의 이름을 검색하는 함수
 void saveData(Product *s, int count); // File에 데이터를 저장하는 함수
-
+int  loadData(Product *s); // File에서 데이터를 불러오는 함수 
