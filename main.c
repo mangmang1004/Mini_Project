7f292607adf295065a21ba32aa490d5617633b56
mangmang1004 <22000669@handong.ac.kr>
2022-04-18 20:33:48 +0900

    original main.c

---
+++ main.c
@@ -0,0 +1,65 @@
+#include "manager.h"
+
+int main(void){
+    Product slist[100];
+    int curcount = 0;
+    int count = 0, menu;
+
+    //count = loadData(slist);
+    curcount = count;
+ 
+    while (1){
+        menu = selectMenu();
+
+        if (menu == 0) break;
+        if (menu == 1){
+            if(count > 0){
+                listProducts(slist,curcount);
+            }else{
+                printf("데이터가 없습니다! \n");
+            }
+        }
+        else if (menu == 2){
+            count += createProduct(&slist[curcount]);
+            curcount ++;
+        }
+        else if (menu == 3){
+            int no = selectDataNo(slist,curcount);
+            if(no == 0){
+                printf("=> 취소됨 ! \n");
+                continue;
+            }
+            updateProduct(&slist[no-1]);
+        }
+        else if (menu == 4){
+            int no = selectDataNo(slist,curcount);
+            if(no == 0){
+                printf("=> 취소됨 ! \n");
+                continue;
+            }
+
+            int deleteok;
+            printf("정말로 삭제하시겠습니까? (삭제 : 1)");
+            scanf("%d", &deleteok);
+            if(deleteok == 1){
+                if(deleteProduct(&slist[no-1])){
+                    count--;
+                }
+            }
+        }
+        else if (menu == 5){
+            if(count == 0)
+                printf("=> 데이터 없음!\n");
+            //else 
+                //saveData(slist, curcount);
+        }
+        else if (menu == 6){
+            if(count == 0)
+                printf("=> 데이터 없음!\n");
+            //else 
+                //searchName(slist, curcount);
+        }
+    }
+    printf("종료됨!\n");
+    return 0;
+}
